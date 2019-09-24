99c29de90ac9d37a7e19df25a7ec6b634f778251
Mahadevan007 <42490734+Mahadevan007@users.noreply.github.com>
2019-09-24 20:16:35 +0530

    adding files for github pages

---
+++ Desktop/portfolio/react/monsters-rolodex/src/App.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+import { Cardlist } from "./components/card-list/card-list.component";
+import { Searchbox } from "./components/searchbox/search-box.component";
+import "./App.css";
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [],
+      searchField: ""
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }));
+  }
+
+  handleChange = e => {
+    this.setState({ searchField: e.target.value });
+  };
+
+  render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+
+    return (
+      <div className="App">
+        <h1>Monsters Rolodex</h1>
+        <Searchbox
+          placeholder="Search monsters"
+          handleChange={this.handleChange}
+        />
+        <Cardlist monsters={filteredMonsters}></Cardlist>
+      </div>
+    );
+  }
+}
+
+export default App;
