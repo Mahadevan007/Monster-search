090acb54f5b7ed402830df93eac7d011e40e673f
Mahadevan007 <42490734+Mahadevan007@users.noreply.github.com>
2019-09-24 20:15:21 +0530

    Updates

---
+++ precache-manifest.55617fdecf7b4a2b7729a452e28d3948.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75730c316354eddacac59a7436d1ed07",
+    "url": "/Monster-search/index.html"
+  },
+  {
+    "revision": "c273b47c74b47d2c39b6",
+    "url": "/Monster-search/static/css/main.c48c009a.chunk.css"
+  },
+  {
+    "revision": "46012db1d69cedf8d1ab",
+    "url": "/Monster-search/static/js/2.3d8af1b9.chunk.js"
+  },
+  {
+    "revision": "c273b47c74b47d2c39b6",
+    "url": "/Monster-search/static/js/main.a60fa950.chunk.js"
+  },
+  {
+    "revision": "784e3cfed62be759580c",
+    "url": "/Monster-search/static/js/runtime-main.f395a688.js"
+  }
+]);
